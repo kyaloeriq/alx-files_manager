5950d324970e9afd6917060540fa9fe13e71b53b
kyaloeriq <kyaloeriq@gmail.com>
2024-09-28 19:48:58 +0000

    getMe method in the UsersController to retrieve user information based on the token

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,8 @@
 // controllers/UsersController.js
 
 import crypto from 'crypto';
-import dbClient from '../utils/db';
+import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis.js'; // Import your Redis client
 
 class UsersController {
   static async postNew(req, res) {
@@ -38,6 +39,28 @@ class UsersController {
       console.error('Error creating new user:', error);
       return res.status(500).json({ error: 'Error creating new user' });
     }
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key); // Get the user ID from Redis
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({ _id: userId });
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    // Return the user object with email and ID
+    return res.status(200).json({ id: user._id, email: user.email });
   }
 }
 
