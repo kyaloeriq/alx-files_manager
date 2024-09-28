b74ccdea644195bfc45db689e6e7ebc9e3dc0f23
kyaloeriq <kyaloeriq@gmail.com>
2024-09-28 17:11:56 +0000

    new route for user creation

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@
 
 import { Router } from 'express';
 import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
 
 const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
+// New route for creating a user
+router.post('/users', UsersController.postNew);
 
 export default router;
