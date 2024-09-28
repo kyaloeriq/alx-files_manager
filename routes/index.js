99ad2753c3db7ac4165b3b43732fcf472ac76803
kyaloeriq <kyaloeriq@gmail.com>
2024-09-28 19:39:07 +0000

    new routes for connecting, disconnecting, and retrieving user info

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 // routes/index.js
 
-import { Router } from 'express';
+import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 
-const router = Router();
+const router = express.Router();
 
+// Existing routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-// New route for creating a user
+// New user-related routes
 router.post('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
