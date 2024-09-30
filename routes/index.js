1609c9ed4e2ba5b80e14741d8a6568ca8be0be9f
kyaloeriq <kyaloeriq@gmail.com>
2024-09-30 16:04:47 +0000

    new endpoint POST /files in the routes/index.js file

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,18 @@
 // routes/index.js
 
 import express from 'express';
+import FilesController from '../controllers/FilesController.js';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
 import AuthController from '../controllers/AuthController.js';
 
 const router = express.Router();
 
-// Existing routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
 // New user-related routes
+router.post('/files', FilesController.postUpload);
 router.post('/users', UsersController.postNew);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
