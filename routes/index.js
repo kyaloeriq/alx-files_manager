7ba412ce3a582984e9e2f35654f9992b22889d28
kyaloeriq <kyaloeriq@gmail.com>
2024-10-01 08:21:30 +0000

    new route for handling POST /users

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,14 @@
 // routes/index.js
-import express from 'express';
-import FilesController from '../controllers/FilesController.js';
-import UsersController from '../controllers/UsersController.js';
-import AuthController from '../controllers/AuthController.js';
 
-const router = express.Router();
+import { Router } from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
-router.post('/files', FilesController.postUpload);
+const router = Router();
+
+// Define the endpoints and map them to controller methods
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
-router.get('/connect', AuthController.getConnect);
-router.get('/disconnect', AuthController.getDisconnect);
-router.get('/users/me', UsersController.getMe);
 
 export default router;
