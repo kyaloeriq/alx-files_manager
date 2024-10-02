a1cd38c17f98ad56a6408e1aeff8ae46bf4d8d12
kyaloeriq <kyaloeriq@gmail.com>
2024-10-02 07:06:46 +0000

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 // routes/index.js
 
-import { Router } from 'express';
 import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
