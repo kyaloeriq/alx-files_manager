428e0d6cd563e5107c9b72e9464db52d49c67e6e
kyaloeriq <kyaloeriq@gmail.com>
2024-10-02 08:27:53 +0000

    index.js

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,20 @@ import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
 
 const router = express.Router();
+const express = require('express');
+const AuthController = require('../controllers/AuthController');
+const UsersController = require('../controllers/UsersController');
 
 // Define the endpoints and map them to controller methods
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+
+// Authentication routes
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+
+// User routes
+router.get('/users/me', UsersController.getMe);
 
 export default router;
